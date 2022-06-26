bd6a320ac55d83ef9d5df26919a96b4e10160ffe
JusteTheCoder <73649494+JusteTheCoder@users.noreply.github.com>
2022-06-27 00:43:31 +0300

    Added weak function to TableUtil

--- src/Internal/Utility/TableUtil/init.lua
+++ src/Internal/Utility/TableUtil/init.lua
@@ -5,8 +5,27 @@
 	Holds [Dictionary], [Array], and [Set] classes.
 ]=]
 
+local WEAK_KEYS_METATABLE = {
+	__mode = "k",
+}
+
+--[=[
+	Use weak keys for a table.
+
+	@within TableUtil
+	@param table {any: any}
+
+	@return table {any: any}
+]=]
+
+local function weak(t)
+	return setmetatable(t, WEAK_KEYS_METATABLE)
+end
+
 return table.freeze({
 	dictionary = require(script.Dictionary),
 	array = require(script.Array),
 	set = require(script.Set),
+
+	weak = weak,
 })
